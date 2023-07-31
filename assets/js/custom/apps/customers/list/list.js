41887c864d78f5ae8b4ff0a07d12fe65dfb2d46c
AlazarG19 <alazargetachew70@gmail.com>
2023-07-31 17:06:19 +0300

    added edit package

--- assets/js/custom/apps/customers/list/list.js
+++ assets/js/custom/apps/customers/list/list.js
@@ -4,8 +4,6 @@
 var KTCustomersList = function () {
     // Define shared variables
     var datatable;
-    var filterMonth;
-    var filterPayment;
     var table
 
     // Private functions
@@ -42,39 +40,6 @@ var KTCustomersList = function () {
         const filterSearch = document.querySelector('[data-kt-customer-table-filter="search"]');
         filterSearch.addEventListener('keyup', function (e) {
             datatable.search(e.target.value).draw();
-        });
-    }
-
-    // Filter Datatable
-    var handleFilterDatatable = () => {
-        // Select filter options
-        filterMonth = $('[data-kt-customer-table-filter="month"]');
-        filterPayment = document.querySelectorAll('[data-kt-customer-table-filter="payment_type"] [name="payment_type"]');
-        const filterButton = document.querySelector('[data-kt-customer-table-filter="filter"]');
-
-        // Filter datatable on submit
-        filterButton.addEventListener('click', function () {
-            // Get filter values
-            const monthValue = filterMonth.val();
-            let paymentValue = '';
-
-            // Get payment value
-            filterPayment.forEach(r => {
-                if (r.checked) {
-                    paymentValue = r.value;
-                }
-
-                // Reset payment value if "All" is selected
-                if (paymentValue === 'all') {
-                    paymentValue = '';
-                }
-            });
-
-            // Build filter string from filter options
-            const filterString = monthValue + ' ' + paymentValue;
-
-            // Filter datatable --- official docs reference: https://datatables.net/reference/api/search()
-            datatable.search(filterString).draw();
         });
     }
 
@@ -260,23 +225,7 @@ var KTCustomersList = function () {
     }
     // 
 
-    // Reset Filter
-    var handleResetForm = () => {
-        // Select reset button
-        const resetButton = document.querySelector('[data-kt-customer-table-filter="reset"]');
 
-        // Reset datatable
-        resetButton.addEventListener('click', function () {
-            // Reset month
-            filterMonth.val(null).trigger('change');
-
-            // Reset payment type
-            filterPayment[0].checked = true;
-
-            // Reset datatable --- official docs reference: https://datatables.net/reference/api/search()
-            datatable.search('').draw();
-        });
-    }
 
     // Init toggle toolbar
     var initToggleToolbar = () => {
@@ -393,9 +342,7 @@ var KTCustomersList = function () {
             initCustomerList();
             initToggleToolbar();
             handleSearchDatatable();
-            handleFilterDatatable();
             handleDeleteRows();
-            handleResetForm();
             handleRenewRows();
         }
     }
--- sidebar.php
+++ sidebar.php
@@ -3,8 +3,8 @@
     <!--begin::Logo-->
     <div class="app-sidebar-logo px-6" id="kt_app_sidebar_logo">
         <!--begin::Logo image-->
-        <a href="../../demo1/dist/index.html">
-            <img alt="Logo" src="assets/media/logos/default-dark.svg" class="h-30px app-sidebar-logo-default" />
+        <a href="#">
+            <img alt="Logo" src="assets/media/avatars/blank.png" class="h-30px app-sidebar-logo-default" />
         </a>
         <!--end::Logo image-->
         <!--begin::Sidebar toggle-->
@@ -30,21 +30,24 @@
                 <!--begin:Menu item-->
                 <div data-kt-menu-trigger="click" class="menu-item here menu-accordion">
                     <!--begin:Menu link-->
-                    <span class="menu-link">
-                        <span class="menu-icon">
-                            <!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
-                            <span class="svg-icon svg-icon-2">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <rect x="2" y="2" width="9" height="9" rx="2" fill="currentColor" />
-                                    <rect opacity="0.3" x="13" y="2" width="9" height="9" rx="2" fill="currentColor" />
-                                    <rect opacity="0.3" x="13" y="13" width="9" height="9" rx="2" fill="currentColor" />
-                                    <rect opacity="0.3" x="2" y="13" width="9" height="9" rx="2" fill="currentColor" />
-                                </svg>
+                    <a class="menu-item here" href="../gym%20management%20system/index.php">
+
+                        <span class="menu-link here">
+                            <span class="menu-icon">
+                                <!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
+                                <span class="svg-icon svg-icon-2">
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <rect x="2" y="2" width="9" height="9" rx="2" fill="currentColor" />
+                                        <rect opacity="0.3" x="13" y="2" width="9" height="9" rx="2" fill="currentColor" />
+                                        <rect opacity="0.3" x="13" y="13" width="9" height="9" rx="2" fill="currentColor" />
+                                        <rect opacity="0.3" x="2" y="13" width="9" height="9" rx="2" fill="currentColor" />
+                                    </svg>
+                                </span>
+                                <!--end::Svg Icon-->
                             </span>
-                            <!--end::Svg Icon-->
+                            <span class="menu-title">Dashboards</span>
                         </span>
-                        <span class="menu-title">Dashboards</span>
-                    </span>
+                    </a>
                     <!--end:Menu link-->
                 </div>
                 <!--end:Menu item-->
@@ -81,7 +84,7 @@
                         <!--begin:Menu item-->
                         <div class="menu-item here">
                             <!--begin:Menu link-->
-                            <a class="menu-link" href="../../demo1/dist/pages/user-profile/projects.html">
+                            <a class="menu-link" href="../gym%20management%20system/add_member.php">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
@@ -119,7 +122,7 @@
                         <!--begin:Menu item-->
                         <div class="menu-item here">
                             <!--begin:Menu link-->
-                            <a class="menu-link" href="../../demo1/dist/pages/team.html">
+                            <a class="menu-link" href="../gym%20management%20system/add_packages.php">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
@@ -141,8 +144,26 @@
     <!--end::sidebar menu-->
     <!--begin::Footer-->
     <div class="app-sidebar-footer flex-column-auto pt-2 pb-6 px-6" id="kt_app_sidebar_footer">
-        <a href="https://preview.keenthemes.com/html/keen/docs" class="btn btn-flex flex-center btn-custom btn-primary overflow-hidden text-nowrap px-0 h-40px w-100" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss-="click" title="200+ in-house components and 3rd-party plugins">
+        <a href="https://alazargetachew.com/" class="btn btn-flex flex-center btn-custom btn-primary overflow-hidden text-nowrap px-0 h-40px w-100" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss-="click" title="Check Out My Website">
             <span class="btn-label">Created By Alazar Getachew</span>
+            <!--begin::Svg Icon | path: icons/duotune/general/gen005.svg-->
+            <span class="svg-icon btn-icon svg-icon-2 m-0">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path opacity="0.3" d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22ZM12.5 18C12.5 17.4 12.6 17.5 12 17.5H8.5C7.9 17.5 8 17.4 8 18C8 18.6 7.9 18.5 8.5 18.5L12 18C12.6 18 12.5 18.6 12.5 18ZM16.5 13C16.5 12.4 16.6 12.5 16 12.5H8.5C7.9 12.5 8 12.4 8 13C8 13.6 7.9 13.5 8.5 13.5H15.5C16.1 13.5 16.5 13.6 16.5 13ZM12.5 8C12.5 7.4 12.6 7.5 12 7.5H8C7.4 7.5 7.5 7.4 7.5 8C7.5 8.6 7.4 8.5 8 8.5H12C12.6 8.5 12.5 8.6 12.5 8Z" fill="currentColor" />
+                    <rect x="7" y="17" width="6" height="2" rx="1" fill="currentColor" />
+                    <rect x="7" y="12" width="10" height="2" rx="1" fill="currentColor" />
+                    <rect x="7" y="7" width="6" height="2" rx="1" fill="currentColor" />
+                    <path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" fill="currentColor" />
+                </svg>
+            </span>
+            <!--end::Svg Icon-->
+        </a>
+    </div>
+    <!--end::Footer-->
+    <!--begin::Footer-->
+    <div class="app-sidebar-footer flex-column-auto pt-2 pb-6 px-6" id="kt_app_sidebar_footer">
+        <a href="https://alazargetachew.com/" class="btn btn-flex flex-center btn-custom btn-primary overflow-hidden text-nowrap px-0 h-40px w-100" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss-="click" title="Contact Me">
+            <span class="btn-label">0943080509</span>
             <!--begin::Svg Icon | path: icons/duotune/general/gen005.svg-->
             <span class="svg-icon btn-icon svg-icon-2 m-0">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
